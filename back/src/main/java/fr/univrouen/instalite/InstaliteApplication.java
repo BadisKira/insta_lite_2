988cea61aebd65f48605bb54f81defd712c5d661
Islam Mokrane <islammkr01@gmail.com>
2023-11-02 16:29:28 +0100

    feat: first commit

---
+++ back/src/main/java/fr/univrouen/instalite/InstaliteApplication.java
@@ -0,0 +1,13 @@
+package fr.univrouen.instalite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaliteApplication.class, args);
+	}
+
+}
